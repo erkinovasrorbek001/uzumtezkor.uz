748139117f788ea177aaeff1579838555470cc65
erkinovasrorbek001 <erkinovasrorbek3@gmail.com>
2025-08-26 15:13:59 +0500

    asror

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
+  plugins: [react(),
+
+    tailwindcss()
   ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
